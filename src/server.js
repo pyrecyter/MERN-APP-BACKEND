4477578f6a5e493c84ad06e5396879c254993516
Pyrecyter <sachiranuwanga123@gmail.com>
2025-07-25 18:27:07 +0530

    installed cors

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import routes from "./routes/index.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
